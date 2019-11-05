225121deaf4ad5cab6e0c85ca3813472c734642d
Sbasquil <scott.basquil@gmail.com>
2019-11-05 13:46:40 +1100

    added change handler for the radio buttons. at the moment im logging the state variable but it seems to be one behind.

--- src/components/PriceCheckForm.js
+++ src/components/PriceCheckForm.js
@@ -7,11 +7,14 @@ class PriceCheckForm extends Component {
     constructor(props){
         super(props)
         this.state = {
-            postcode: ""
+            postcode: "",
+            selectedCategory: "",
+
         }
 
         this.handleSubmit = this.handleSubmit.bind(this);
         this.handlePostcodeChange = this.handlePostcodeChange.bind(this);
+        this.handleCategoryChange = this.handleCategoryChange.bind(this);
         this.fetchSuppliers = this.fetchSuppliers.bind(this)
     }
 
@@ -28,13 +31,19 @@ class PriceCheckForm extends Component {
 
     handlePostcodeChange(e) {
         this.setState({postcode: e.target.value})
+        
+    }
+
+    handleCategoryChange(e) {
+        this.setState({selectedCategory: e.target.value})
+        console.log(`${this.state.selectedCategory}`)
     }
 
 
 
     render() {
         const categoryRadioSelectors = categories.map( category => 
-            <div key={category.catId}>
+            <div key={category.catId} onChange={this.handleCategoryChange}>
                 <input type="radio" id={category.catId} name="category" value={category.value}/>
                 <label for={category.catId}>{category.displayName}</label>
             </div>
@@ -46,8 +55,12 @@ class PriceCheckForm extends Component {
                     <input type="text" value={this.state.postcode} onChange={this.handlePostcodeChange}/>
                     <input type="submit" value="Check your Postcode"/>
                     <div className="categorySelection">
+                        {/* need to find a way to handle the change in radio buttons and set the state */}
                         Select a product category to compare prices.
                         {categoryRadioSelectors}
+                    </div>
+                    <div className="productSearch">
+                        What are you looking for
                     </div>
                 </form>
             </div>
