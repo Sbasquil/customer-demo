3fb2260aa95f0252028797f48550a20dde1b5737
Sbasquil <scott.basquil@gmail.com>
2019-11-05 13:18:50 +1100

    added radio button selectors for selecting the category in the PriceCheckForm.

--- src/components/PriceCheckForm.js
+++ src/components/PriceCheckForm.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
-import axios from 'axios'
+import axios from 'axios';
+import { categories } from '../utils/Constants.js';
 
 
 class PriceCheckForm extends Component {
@@ -10,7 +11,7 @@ class PriceCheckForm extends Component {
         }
 
         this.handleSubmit = this.handleSubmit.bind(this);
-        this.handleChange = this.handleChange.bind(this);
+        this.handlePostcodeChange = this.handlePostcodeChange.bind(this);
         this.fetchSuppliers = this.fetchSuppliers.bind(this)
     }
 
@@ -25,19 +26,29 @@ class PriceCheckForm extends Component {
     }
 
 
-    handleChange(e) {
+    handlePostcodeChange(e) {
         this.setState({postcode: e.target.value})
     }
 
 
 
     render() {
+        const categoryRadioSelectors = categories.map( category => 
+            <div key={category.catId}>
+                <input type="radio" id={category.catId} name="category" value={category.value}/>
+                <label for={category.catId}>{category.displayName}</label>
+            </div>
+        ) 
         return (
             <div className="PriceCheckForm">
                 <form onSubmit={this.handleSubmit}>
                     Enter your Postcode:
-                    <input type="text" value={this.state.postcode} onChange={this.handleChange}/>
+                    <input type="text" value={this.state.postcode} onChange={this.handlePostcodeChange}/>
                     <input type="submit" value="Check your Postcode"/>
+                    <div className="categorySelection">
+                        Select a product category to compare prices.
+                        {categoryRadioSelectors}
+                    </div>
                 </form>
             </div>
         )
